b3f517b3af35459c515c09190d0cb0c7b806cb7f
HassanAmer11 <hassan.amer.2554@gmail.com>
2026-02-17 00:02:17 +0200

    ...

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -55,7 +55,7 @@ export default function Footer() {
                   >
                     <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
                   </svg>
-                  <span>(+20) 01093333333</span>
+                  <span>(+2) 01152566813</span>
                 </div>
                 <div className="flex items-center text-gray-600 text-sm">
                   <svg
@@ -74,7 +74,7 @@ export default function Footer() {
                     <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
                     <rect x={2} y={4} width={20} height={16} rx={2} />
                   </svg>
-                  <span>support@shopmart.com</span>
+                  <span>hassan.amer.dev@gmail.com</span>
                 </div>
               </div>
             </div>
